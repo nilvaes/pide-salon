97ae319186443a75c639fe460a2dc3e2e60dd0e5
Ömer <savas.qrx@gmail.com>
2026-04-06 23:30:20 +0200

    footer padding change - make it thinner

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
   const { footer } = content;
 
   return (
-    <footer className="border-t border-bosporus-red/20 bg-bosporus-red-dark py-10 text-bosporus-cream/90">
+    <footer className="border-t border-bosporus-red/20 bg-bosporus-red-dark py-5 text-bosporus-cream/90 sm:py-6">
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
         <p className="text-center text-sm text-bosporus-cream/70">
           {footer.legal[lang]}
